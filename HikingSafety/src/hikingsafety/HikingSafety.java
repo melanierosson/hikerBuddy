82a14c5b3c3c26d61058186728457328dd1ca71c
melanierosson <melanie.rosson@gmail.com>
2016-12-05 20:07:08 -0800

    pushing basic GUI layout for edits

---
+++ HikingSafety/src/hikingsafety/HikingSafety.java
@@ -0,0 +1,17 @@
+/*
+ * CIS 043 Term Project
+ * Hiking Safety
+ * Group 7:
+ * Melanie Rosson
+ * Amanda Bukur
+ * Richard Inocencio
+ */
+package hikingsafety;
+
+public class HikingSafety {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
