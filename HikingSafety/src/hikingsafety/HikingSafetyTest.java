5ae1272123b313510c63e5c7d7b53cd984749608
melanierosson <melanie.rosson@gmail.com>
2016-12-07 19:16:49 -0800

    removing unnecessary comments

--- HikingSafety/src/hikingsafety/HikingSafetyTest.java
+++ HikingSafety/src/hikingsafety/HikingSafetyTest.java
@@ -17,15 +17,6 @@ public class HikingSafetyTest {
         java.awt.EventQueue.invokeLater(() -> {
             new TabbedForm().setVisible(true);
         });
-        
-//        HikingSafety hiker1 = new HikingSafety("Bob", "Watt", 125.0, 63.0);
-//        
-//        Scanner input = new Scanner(System.in);
-//        
-//        while (int i <= 5){
-//            calcBMIInt1 = 
-//        }
-//        
-//       System.out.printf("%s %s has a BMI of %f%n", hiker1.getFname(), hiker1.getLname(), hiker1.bmiCalculator());
+
     }
 }
